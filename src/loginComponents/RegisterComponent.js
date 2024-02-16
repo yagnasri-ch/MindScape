aac83ddb7402a07562436b3c73def109eda4699b
alekhyanalluri <nallurialekhya6@gmail.com>
2024-02-15 21:42:53 -0600

    added css files

--- src/loginComponents/RegisterComponent.js
+++ src/loginComponents/RegisterComponent.js
@@ -1,37 +1,39 @@
 import React from 'react';
-import '../css/login.css'; // Check the correct path for CSS file
+import '../loginComponents/RegisterComponent.css';
 
 const RegisterComponent = () => {
     return (
-            <div className="login">
-                <form action="" className="login__form">
-                    <h1 className="login__title">Register</h1>
-                    <div className="login__content">
-                        <div className="login__box">
-                            <i className="ri-user-3-line login__icon"></i>
-                            <div className="login__box-input">
-                                <input type="email" required className="login__input" id="register-email" placeholder=" "/>
-                                <label htmlFor="register-email" className="login__label">Email</label>
-                            </div>
-                        </div>
-                        <div className="login__box">
-                            <i className="ri-lock-2-line login__icon"></i>
-                            <div className="login__box-input">
-                                <input type="password" required className="login__input" id="register-pass" placeholder=" "/>
-                                <label htmlFor="register-pass" className="login__label">Password</label>
-                                <i className="ri-eye-off-line login__eye" id="register-eye"></i>
-                            </div>
+        <div className="register">
+            <form action="" className="register__form">
+                <h1 className="register__title">Register</h1>
+                <div className="register__content">
+                    <div className="register__box">
+                        <i className="ri-user-3-line register__icon"></i>
+                        <div className="register__box-input">
+                            <input type="email" required className="register__input" id="register-email" placeholder=" "/>
+                            <label htmlFor="register-email" className="register__label">Email</label>
                         </div>
                     </div>
-                    <div className="login__check">
-                        <div className="login__check-group">
-                            <input type="checkbox" className="login__check-input" id="register-check"/>
-                            <label htmlFor="register-check" className="login__check-label">I agree to the terms and conditions</label>
+                    <div className="register__box">
+                        <i className="ri-lock-2-line register__icon"></i>
+                        <div className="register__box-input">
+                            <input type="password" required className="register__input" id="register-pass" placeholder=" "/>
+                            <label htmlFor="register-pass" className="register__label">Password</label>
+                            <i className="ri-eye-off-line register__eye" id="register-eye"></i>
                         </div>
                     </div>
-                    <button type="submit" className="login__button">Register</button>
-                </form>
-            </div>
+                </div>
+
+                <div className="register__check">
+                    <div className="register__check-group">
+                        <input type="checkbox" className="register__check-input" id="register-check"/>
+                        <label htmlFor="register-check" className="register__check-label">I agree to the terms and
+                            conditions</label>
+                    </div>
+                </div>
+                <button type="submit" className="register__button">Register</button>
+            </form>
+        </div>
     );
 }
 
