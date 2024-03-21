fff39a77ceb3ead6be23cbf2035b91381a62a8e4
Yagnasri <yagnasrich@gmail.com>
2024-03-21 15:14:48 -0500

    Deleted unused <Introduction> component

--- src/loginComponents/FirstPage.js
+++ src/loginComponents/FirstPage.js
@@ -1,13 +1,11 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './FirstPage.css'; // Assuming the CSS file is in the same directory as this component
 import IntroText from "./IntroText";
-import Introduction from "./Introduction"; // Import Introduction component if it's meant to be used
 
 const FirstPage = () => {
     return (
         <div>
             <IntroText />
-            <Introduction/>
         </div>
     );
 };
